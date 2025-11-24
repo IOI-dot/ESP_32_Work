342f4af82d631c1315fa27a9878fe2095d2c6589
Omar <omarelhussein@aucegypt.edu>
2025-11-24 13:58:57 +0200

    Update Weather_Test.ino

--- Weather_Test.ino
+++ Weather_Test.ino
@@ -1,30 +1,46 @@
 #include <WiFi.h>
+#include "esp_wpa2.h"   
+#include "esp_wifi.h"
+#include "esp_err.h"
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-const char* ssid     = "realme C15";
-const char* password = "123456789";
-const String apiKey  = "8b04f782bc364003b8d102854252311";  
+// WiFi credentials
+const char* ssid     = "AUC-WiFi";
+const char* username = "OmarElHussein";     
+const char* password = "Hank05041321567890*"; 
 
-const String query = "Cairo";
+// WeatherAPI
+const String apiKey  = "8b04f782bc364003b8d102854252311";  
+const String query   = "30.0192,31.5029";      //AUC Bus Gate
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
 
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_STA);
+
+  // WPA2 Enterprise setup
+  esp_wifi_sta_wpa2_ent_set_identity((uint8_t*)username, strlen(username));
+  esp_wifi_sta_wpa2_ent_set_username((uint8_t*)username, strlen(username));
+  esp_wifi_sta_wpa2_ent_set_password((uint8_t*)password, strlen(password));
+  esp_wifi_sta_wpa2_ent_enable();
+
+  WiFi.begin(ssid);
+
+  Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
-    delay(300);
+    delay(500);
     Serial.print(".");
   }
-  Serial.println("\nWiFi Connected!");
+  Serial.println("\nConnected");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-
   if (WiFi.status() == WL_CONNECTED) {
-
     HTTPClient http;
 
     String url = "https://api.weatherapi.com/v1/current.json?key=" +
@@ -47,17 +63,21 @@ void loop() {
 
       Serial.print("Condition Code: ");
       Serial.println(code);
+
       if (precip > 0.0) {
         Serial.println("RAIN");
       } else {
         Serial.println("NO RAIN");
       }
-
     } else {
-      Serial.println("Error fetching WeatherAPI data");
+      Serial.print("Error fetching WeatherAPI data. HTTP code: ");
+      Serial.println(httpResponseCode);
     }
 
     http.end();
+  } else {
+    Serial.println("WiFi disconnected");
+    WiFi.begin(ssid);
   }
 
   delay(5000);  
